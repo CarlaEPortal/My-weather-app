4565997db2520758ea76bf556be180a114088068
CarlaEPortal <cep.psicoterapia@gmail.com>
2023-04-24 16:25:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,10 +33,12 @@ let hours = now.getHours();
 if (hours < 10) {
   hours = `0${hours}`;
 }
+
 let minutes = now.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
+
 let currentDate = document.querySelector("#date");
 currentDate.innerHTML = `${day}, ${month} ${date}, ${year}  ${hours}:${minutes}`;
 
@@ -50,6 +52,14 @@ function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#input-City").value;
   search(city);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "ed55b36e362d8733f7d859247cedeaf2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showWeather(response) {
@@ -80,6 +90,8 @@ function showWeather(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function currentPosition(position) {
@@ -117,7 +129,8 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -150,4 +163,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Utrecht");
-displayForecast();
